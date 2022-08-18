d51567a92d649cf567f415f27d57ccbedbcfe3d6
Millet520 <zhangdada5200@gmail.com>
2022-08-18 11:55:36 +0800

    decorator

--- note/views.py
+++ note/views.py
@@ -6,6 +6,20 @@ from note.models import Note
 from user.models import User
 
 
+def check_login(fn):
+    def wrap(request, *args, **kwargs):
+        if 'username' not in request.session or 'uid' not in request.session:
+            c_username = request.COOKIES.get('username')
+            c_uid = request.COOKIES.get('uid')
+            if not c_username or not c_uid:
+                return HttpResponseRedirect('/user/login')
+            else:
+                request.session['username'] = c_username
+                request.session['uid'] = c_uid
+        return fn(request, *args, **kwargs)
+    return wrap
+
+
 def list_view(request):
     try:
         notes = Note.objects.all()
@@ -16,6 +30,7 @@ def list_view(request):
     return render(request, 'note/list_note.html', locals())
 
 
+@check_login
 def add_view(request):
     if request.method == "GET":
         return render(request, 'note/add_note.html')
